64dddfd6c1ae35298f82e1d7fcef39644d502af3
Eric Booker <ericbooker12@gmail.com>
2019-02-20 15:13:45 -0800

    Create data in csv format and create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+file = "db/project_locations.csv"
+
+first = true
+
+CSV.foreach(file) do |row|
+	if first
+		puts row
+	else
+		Project.create(
+			name: row[0],
+			project_number: row[1],
+			address: row[2],
+			city:[3],
+			county:[4],
+			state:[5],
+			zip:[6],
+			apn:[7],
+			latitude: row[8],
+			longitude: row[9],
+			elevation: row[10]
+		)
+		puts row
+	end
+end
