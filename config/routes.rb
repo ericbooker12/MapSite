7290363db51fd3c61253ae47deb387149ceacd3f
Eric Booker <ericbooker12@gmail.com>
2019-02-20 16:09:23 -0800

    Controller and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'projects/index'
-  get 'projects/new'
-  get 'projects/show'
-  get 'projects/create'
-  get 'projects/edit'
-  get 'projects/update'
+
+	root 'projects#index'
+
+  resources :projects
+
   get 'projects/delete'
-  get 'projects/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
